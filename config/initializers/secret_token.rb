942f963a7fb01803688a106fac6a597300376e3e
Hiroyuki Inoue <mammymax@gmail.com>
2012-05-20 15:13:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SparqlShare::Application.config.secret_token = 'f8ca82c09356046a641a188e7dedf19f237054e212fbb9599e640a85af107e15535714d24c23e5eb660f8aa47ee19d119aa682c3e654a100c4526b9e6b697dc1'
